3d29ca00558b7ad455a97cc12043dc66a534ce5c
TheCodingStudent <a360069@uach.mx>
2023-07-26 20:09:14 -0600

    "Package Structure"

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+VERSION = "0.0.1"
+DESCRIPTION = "Package for User Interface"
+LONG_DESCRIPTION = "Package for User Interface"
+
+setup(
+    name="UIron",
+    version=VERSION,
+    author="Armando Chaparro",
+    author_email="pylejandria@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
