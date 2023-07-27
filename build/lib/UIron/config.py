0d9a031cfeeba7572fac00c5e5812ea1750e25df
TheCodingStudent <a360069@uach.mx>
2023-07-27 13:25:54 -0600

    Build 0.0.3

--- build/lib/UIron/config.py
+++ build/lib/UIron/config.py
@@ -1,16 +1,18 @@
 import json
-
+from UIron.utils import prettify
 
 class ConfigError(Exception):
     ...
 
 
 class Config:
-    def __init__(self, config_path: str):
+    def __init__(self, config_path: str, auto_save: bool=True, indent: int=4):
         
         # PROPERTIES
+        self.indent = indent
         self.path = config_path
         self.config = self.load()
+        self.auto_save = auto_save
     
     def load(self) -> None:
         """Loads the configutation from json to dictionary"""
@@ -23,8 +25,15 @@ class Config:
     def save(self) -> None:
         """Saves the configuration based on dictionary"""
         with open(self.path, 'w') as f:
-            json.dump(self.config, f)
+            json.dump(self.config, f, indent=self.indent)
     
     def __getitem__(self, key: str) -> object:
         if not key in self.config: raise ConfigError(f'Attribute "{key}" does not exist.')
         return self.config[key]
+    
+    def __setitem__(self, key: str, value: object) -> None:
+        self.config[key] = value
+        if self.auto_save: self.save()
+    
+    def __repr__(self) -> None:
+        return prettify(self.config)
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 DESCRIPTION = "Package for User Interface"
 LONG_DESCRIPTION = "Package for User Interface"
 
